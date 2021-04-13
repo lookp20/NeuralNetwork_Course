8deadc47ddda22848b4db92bc8a8e01203721106
Georgii Kostiuchik <georgii.kostiuchik@gmail.com>
2021-04-13 21:20:40 +0200

    AB2 final version

--- NN2.py
+++ NN2.py
@@ -2,41 +2,43 @@
 
 import numpy as np
 import nnwplot
-
-np.random.seed(42)
+import matplotlib.pyplot as plt
 
 class SLN:
     def __init__(self,dIn,cOut):
+        np.random.seed(42)
         self._dIn = dIn
         self._cOut = cOut
         self._W = np.random.randn(cOut,dIn) / np.sqrt(dIn+1)
         self._b = np.zeros(cOut)[np.newaxis].T
+        # Aus Aufgabenblatt 1
         # self._W = np.array([[-.3, 1]])
-        #self._b = np.array([-0.5])
+        # self._b = np.array([0])
         
     def neuron(self, X):
         N = X.shape[1]
         net = np.zeros(N)
-        threshold = 2
+        threshold = 0
     
-        for n in range(0,N):
-            for j in range(0,self._dIn): # input neurons
+        for n in range(0,N): # iterate over instances
+            for j in range(0,self._dIn): # iterate over weights
                  net[n] += self._W[0,j] * X[j, n]
             net[n] += self._b * 1
         return net > threshold
     
     def DeltaTrain(self,X,T,eta,maxIter,maxErrorRate):
         N = X.shape[1]
+        plt.ion()
         for i in range(maxIter):
-            Y = self.neuron(X)
-            err = ErrorRate(Y,T)
+            Y = self.neuron(X) # classify
+            err = ErrorRate(Y,T)  # calculate error rate
             print(err)
-            if err < maxErrorRate:
+            if err < maxErrorRate:  # stop if maxErrorRate reached
                 break
             deltaWkj = np.zeros(3)
-            for j in range(2): # iterate over weights
+            for j in range(self._dIn): # iterate over weights
                 summe = 0
-                for n in range(N): # iterate over features
+                for n in range(N): # iterate over input neurons
                     summe += eta*(T[n]-Y[n])*X[j,n]
                 deltaWkj[j] = 1/N*summe
                 
@@ -49,7 +51,19 @@ class SLN:
             self._W[0,0] += deltaWkj[0]
             self._W[0,1] += deltaWkj[1]
             self._b[0,0] += deltaWkj[2]
+            nnwplot.plotTwoFeatures(X[:2],T,self.neuron)
                 
+'''
+Falls Y ein Vektor ist, dann ist die Fehlerrate gleich der Summe
+der falsch-klassifizierten Instanzen dividiert durch die Anzahl der Instanzen.
+Falls Y eine one-hot Matrix ist, dann werden die Indizes der Maximumwerte
+verglichen, falsch-klassifizierte Instanzen aufsummiert und durch die Anzahl
+der Instanzen dividiert.
+
+One-hot Kodierung
+1 0 0 1 ... -> argmax(0) -> [0, 1, 1, 0, ...] 
+0 1 1 0 ...
+'''
 def ErrorRate(Y,T):
     if Y.ndim==1 or Y.shape[0]==1:
         errors=Y!=T
@@ -59,11 +73,23 @@ def ErrorRate(Y,T):
         return errors.sum()/Y.shape[1]        
 
 
-iris_data = np.loadtxt('/path/to/iris.csv', delimiter=',')
-X = iris_data[:100,:4].T
-T = iris_data[:100,4].T
+#%% Aufgabe 3e
+iris_data = np.loadtxt('iris.csv', delimiter=',') # load iris dataset
+X = iris_data[:100,:4].T # select first 100 instances with 4 features and transpose
+T = iris_data[:100,4].T # select first 100 instances with label and transpose
 
-m=SLN(2,1)
+m=SLN(2,1) # create SLN with 2 input neuron and 1 output neuron
 
-m.DeltaTrain(X[:2],T,0.01,1000,0.02)
-nnwplot.plotTwoFeatures(X[:2],T,m.neuron)
+m.DeltaTrain(X[:2],T,0.01,1000,0.05) # train SLN
+
+#%% Aufgabe 3d
+und_model = SLN(2,1)
+
+train_data = np.array([[0,0,0], [1,0,0], [0,1,0], [1,1,1]])
+X_und = train_data[:,:2].T
+T_und = train_data[:,2].T
+
+und_model.DeltaTrain(X_und,T_und,0.1,1000,0.02) # train SLN
+#%% Aufgabe 3f
+m = SLN(2,1)
+m.DeltaTrain(X[1:3],T,0.01,1000,0.02) # train SLN
